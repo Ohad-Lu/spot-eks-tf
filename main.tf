bf114a6b1953e54cfbf31c67f4825a74d6afec70
Ohad <ohadlouvton@gmail.com>
2022-08-22 21:43:26 +0300

    added subnet, and basic eks

--- main.tf
+++ main.tf
@@ -17,12 +17,21 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "private-subnet" {
+resource "aws_subnet" "private-subnet-01" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-2a"
   tags = {
-    Name = "private-subnet"
+    Name = "private-subnet-us-east-2a"
+  }
+}
+
+resource "aws_subnet" "private-subnet-02" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-2b"
+  tags = {
+    Name = "private-subnet-us-east-2b"
   }
 }
 
@@ -31,7 +40,7 @@ resource "aws_eip" "eip" {
 
 resource "aws_nat_gateway" "example" {
   allocation_id = aws_eip.eip.id
-  subnet_id     = aws_subnet.private-subnet.id
+  subnet_id     = aws_subnet.private-subnet-01.id
 
   tags = {
     Name = "gw NAT"
@@ -52,18 +61,33 @@ resource "aws_route" "private-rt-public-route" {
   nat_gateway_id         = aws_nat_gateway.example.id
 }
 
-resource "aws_route_table_association" "private-subnets-rt-association" {
-  subnet_id      = aws_subnet.private-subnet.id
+resource "aws_route_table_association" "private-subnets-rt-association-01" {
+  subnet_id      = aws_subnet.private-subnet-01.id
   route_table_id = aws_route_table.private-rt.id
 }
 
-resource "aws_subnet" "public-subnet" {
+resource "aws_route_table_association" "private-subnets-rt-association-02" {
+  subnet_id      = aws_subnet.private-subnet-02.id
+  route_table_id = aws_route_table.private-rt.id
+}
+
+resource "aws_subnet" "public-subnet-01" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-us-east-2a"
+  }
+}
+
+resource "aws_subnet" "public-subnet-02" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.4.0/24"
   availability_zone       = "us-east-2b"
   map_public_ip_on_launch = true
   tags = {
-    Name = "public-subnet"
+    Name = "public-subnet-us-east-2b"
   }
 }
 
@@ -92,9 +116,58 @@ resource "aws_route" "public-rt-public-route" {
   gateway_id             = aws_internet_gateway.ig.id
 }
 
-resource "aws_route_table_association" "public-subnets-rt-association" {
-  subnet_id      = aws_subnet.public-subnet.id
+resource "aws_route_table_association" "public-subnets-rt-association-01" {
+  subnet_id      = aws_subnet.public-subnet-01.id
+  route_table_id = aws_route_table.public-rt.id
+}
+
+resource "aws_route_table_association" "public-subnets-rt-association-02" {
+  subnet_id      = aws_subnet.public-subnet-02.id
   route_table_id = aws_route_table.public-rt.id
 }
 
 
+
+resource "aws_eks_cluster" "example" {
+  name     = "ohad-eks-01"
+  role_arn = aws_iam_role.cluster-role.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.private-subnet-01.id, aws_subnet.private-subnet-02.id]
+    endpoint_public_access = true
+    endpoint_private_access = true
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster-role-AmazonEKSClusterPolicy-attach,
+    aws_vpc.main
+  ]
+}
+
+resource "aws_iam_role" "cluster-role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "cluster-role-AmazonEKSClusterPolicy-attach" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster-role.name
+}
+
+
+
+##########
